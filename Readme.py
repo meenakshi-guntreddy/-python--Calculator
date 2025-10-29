5662c19cc4bdb6e27d9e02de0da3f77efdf43ed3
meenakshi-guntreddy <guntreddymeenakshi6715@gmail.com>
2025-10-29 23:15:48 +0530

    Add files via upload

---
+++ Readme.py
@@ -0,0 +1,30 @@
+#  Python CLI Calculator
+
+A simple command-line calculator built using **Python** as part of the **BH Python Developer Internship – Task 1**.
+
+---
+
+## Objective
+To create a calculator that performs basic arithmetic operations through a Command Line Interface (CLI).
+
+---
+
+## Features
+- Addition, Subtraction, Multiplication, and Division
+- Error handling for invalid inputs and division by zero
+- Menu-based interface
+- Loops until the user chooses to exit
+
+---
+
+## Technologies Used
+- Python
+- VS Code / Terminal
+
+---
+
+##  How to Run the Project
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/meenakshi-guntreddy/-python-Calculator.git
+   cd -python-Calculator
